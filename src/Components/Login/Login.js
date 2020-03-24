23a0f0987825b979cb8625d4c2db26a556aed0b8
Erik Raith <raithhtiar@gmail.com>
2020-03-23 23:29:42 -0600

    [feat] Add form validation sad path UI and login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-// import "./Login.scss"
+// import './Login.scss'
 
 const validEmailRegex = RegExp(
-  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  // eslint-disable-next-line
+  /^(([^<>()\[\]\.,;:\s@\']+(\.[^<>()\[\]\.,;:\s@\']+)*)|(\'.+\'))@(([^<>()[\]\.,;:\s@\']+\.)+[^<>()[\]\.,;:\s@\']{2,})$/i
 );
 
 class Login extends Component {
@@ -28,8 +29,8 @@ class Login extends Component {
 
     switch (name) {
       case "userName":
-        errors.fullName =
-          value.length < 5 ? "Full Name must be 5 characters long!" : "";
+        errors.userName =
+          value.length < 5 ? "User Name must be 5 characters long!" : "";
         break;
       case "email":
         errors.email = validEmailRegex.test(value) ? "" : "Email is not valid!";
@@ -45,32 +46,61 @@ class Login extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    if(validateFor)
+    if (this.validateForm(this.state.errors)) {
+      const { login } = this.props;
+      const { userName, email, purpose } = this.state;
+      login({
+        userName: userName,
+        email: email,
+        purpose: purpose
+      });
+      console.info("Valid Form");
+    } else {
+      console.error("Invalid Form");
+    }
+  };
 
-    const { login } = this.props;
-    login(this.state);
+  validateForm = errors => {
+    let valid = true;
+    Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+    return valid;
   };
 
   render() {
+    const { errors, userName, email } = this.state;
+    const isEnabled =
+      errors.email.length === 0 &&
+      errors.userName.length === 0 &&
+      userName !== "" &&
+      email !== "";
+
     return (
       <form>
-        <input
-          type="text"
-          placeholder="User Name"
-          name="userName"
-          value={this.state.userName}
-          onChange={event => this.handleChange(event)}
-        />
+        <div className="input-wrapper">
+          <span className="error">{errors.userName}</span>
+          <input
+            type="text"
+            placeholder="User Name"
+            name="userName"
+            value={this.state.userName}
+            onChange={event => this.handleChange(event)}
+          />
+        </div>
+        
+        <div className="input-wrapper">
+          <span className="error">{errors.email}</span>
+          <input
+            type="text"
+            placeholder="Email@provider.com"
+            name="email"
+            value={this.state.email}
+            onChange={event => this.handleChange(event)}
+          />
+        </div>
 
-        <input
-          type="text"
-          placeholder="Email@provider.com"
-          name="email"
-          value={this.state.email}
-          onChange={event => this.handleChange(event)}
-        />
-
-        <button onClick={this.handleSubmit}>Login</button>
+        <button disabled={!isEnabled} onClick={this.handleSubmit}>
+          Login
+        </button>
       </form>
     );
   }
