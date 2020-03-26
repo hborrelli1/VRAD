44ece11c79a5b53f7c5a1b7990b9f1df70a3efde
Erik Raith <raithhtiar@gmail.com>
2020-03-25 18:04:55 -0600

    [test] Add LocationCard tests

--- src/Components/LocationCard/LocationCard.test.js
+++ src/Components/LocationCard/LocationCard.test.js
@@ -1,9 +1,98 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import LocationCard from './LocationCard';
+import '@testing-library/jest-dom';
 
 describe("Test", () => {
-  it("Should be a placeholder", () => {
+  let listingDetails;
+  let favoriteLocations;
+  beforeEach(() => {
+    listingDetails = {
+            "listing_id": 3,
+            "area_id": 590,
+            "name": "Hip RiNo Party Spot",
+            "address": {
+                "street": "2250 Lawrence St",
+                "zip": "80205"
+            },
+            "details": {
+                "neighborhood_id": 5124122,
+                "superhost": true,
+                "seller_source": "91jss1",
+                "beds": 3,
+                "baths": 2.5,
+                "cost_per_night": 420,
+                "features": [
+                    "hot tub",
+                    "espresso machine"
+                ]
+            },
+            "dev_id": "u4gh2j",
+            "area": "rino",
+            "db_connect": 834470
+        }
+        favoriteLocations = [3];
+  });
 
-  })
+  it('should display the locations info', () => {
+    const mockFavorite = jest.fn();
+    const mockChangeView = jest.fn();
+    const { getByText } = render(
+      <LocationCard
+        favorite={mockFavorite}
+        key={listingDetails.listing_id}
+        data={listingDetails}
+        goToListing={mockChangeView}
+        favoriteLocations = {favoriteLocations}
+      />
+    );
+
+    const locationNameEl = getByText("Hip RiNo Party Spot");
+    const locationAddressEl = getByText("2250 Lawrence St");
+
+    expect(locationNameEl).toBeInTheDocument();
+    expect(locationAddressEl).toBeInTheDocument();
+
+  });
+
+  it('should be able to change views', () => {
+    const mockChangeView = jest.fn();
+    const mockFavorite = jest.fn();
+    const { getByText } = render(
+      <LocationCard
+        favorite={mockFavorite}
+        key={listingDetails.listing_id}
+        data={listingDetails}
+        goToListing={mockChangeView}
+        favoriteLocations = {favoriteLocations}
+      />
+    );
+    const goToListing = getByText('Go to Listing');
+
+    fireEvent.click(goToListing);
+``
+    expect(mockChangeView).toHaveBeenCalledTimes(1);
+    expect(mockChangeView).toHaveBeenCalledWith(3,'LocationListingCard');
+  });
+
+  it('should be able to favorite locations', () => {
+    const mockChangeView = jest.fn();
+    const mockFavorite = jest.fn();
+    const { getByText } = render(
+      <LocationCard
+        favorite={mockFavorite}
+        key={listingDetails.listing_id}
+        data={listingDetails}
+        goToListing={mockChangeView}
+        favoriteLocations = {favoriteLocations}
+      />
+    );
+    const favoriteButton = getByText('favorite');
+
+    fireEvent.click(favoriteButton);
+``
+    expect(mockFavorite).toHaveBeenCalledTimes(1);
+    expect(mockFavorite).toHaveBeenCalledWith(3);
+  });
+
 })
