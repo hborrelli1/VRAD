79c0b863707953f0599192a7832f138f7941fb66
Erik Raith <raithhtiar@gmail.com>
2020-03-29 16:40:58 -0600

    [fix] Reset LoggedIn state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      isLoggedIn: true,
+      isLoggedIn: false,
       userInfo: {
         name: "",
         email: "",
