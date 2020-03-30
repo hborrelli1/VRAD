b5c0ff419d4d01e5a408bac6d36da2fff798f770
Erik Raith <raithhtiar@gmail.com>
2020-03-29 20:11:50 -0600

    [feat][wip] Dynamic breadcrumbs

--- src/Components/Breadcrumb/Breadcrumb.js
+++ src/Components/Breadcrumb/Breadcrumb.js
@@ -6,55 +6,83 @@ const Breadcrumb = props => {
   const routes = [
     {
       path: "/areas/",
-      link:'/',
-      linkText:'Home'
+      link: "/",
+      linkText: "Home"
+    },
+    {
+      path: "/areas/river-north/",
+      link: "/areas/river-north",
+      linkText: "River-North"
     },
     {
       path: "/areas/river-north/:id",
-      link:'/areas/river-north',
-      linkText:'Rino'
+      link: "/areas/river-north",
+      linkText: "River-North-Listing"
+    },
+    {
+      path: "/areas/park-hill/",
+      link: "/areas/park-hill",
+      linkText: "Park-Hill!"
     },
     {
       path: "/areas/park-hill/:id",
-      link:'/areas/park-hill',
-      linkText:'Park-Hill!'
+      link: "/areas/park-hill",
+      linkText: "Park-Hill-Listing!"
+    },
+    {
+      path: "/areas/lower-highlands/",
+      link: "/areas/lower-highlands",
+      linkText: "Lower-Highlands!"
     },
     {
       path: "/areas/lower-highlands/:id",
-      link:'/areas/lower-highlands',
-      linkText:'Lower-Highlands!'
+      link: "/areas/lower-highlands",
+      linkText: "Lower-Highlands-Listing!"
+    },
+    {
+      path: "/areas/capitol-hill/",
+      link: "/areas/capitol-hill",
+      linkText: "Capitol-Hill!"
     },
     {
       path: "/areas/capitol-hill/:id",
-      link:'/areas/capitol-hill',
-      linkText:'Capitol-Hill!'
-    },
-
+      link: "/areas/capitol-hill",
+      linkText: "Capitol-Hill-Listings!"
+    }
   ];
 
   const routeConstruction = () => {
-    let finalRoutes = routes.map(route=>{
-      return (
-        <Route path={route.path}>
-          <li>
-            <Link to={route.link}>{route.linkText}</Link>
-          </li>
-        </Route>
-      )
-    })
-    let lastIndex = props.path.split('/').length -2;
-    finalRoutes = finalRoutes.reduce((acc,child,index) => {
-      const notLast = index< lastIndex;
+    let finalRoutes = routes.map(route => {
+      return {
+        routeHTML: (
+          <Route path={route.path}>
+            <li>
+              <Link to={route.link}>{route.linkText}</Link>
+            </li>
+          </Route>
+        ),
+        endrouteHTML: (
+          <Route path={route.path}>
+            <li>
+              {route.linkText}
+            </li>
+          </Route>
+        )
+      };
+    });
+    let lastIndex = props.path.split("/").length - 2;
+    finalRoutes = finalRoutes.reduce((acc, child, index) => {
+      const notLast = index < lastIndex;
       if (notLast) {
-        acc.push(child,'/')
+        acc.push(child.routeHTML, "/");
       } else {
-        acc.push(child)
+        acc.push(child.endrouteHTML);
       }
       return acc;
-    },[])
+    }, []);
 
     return finalRoutes;
-  }
+  };
 
   return (
     <nav>
