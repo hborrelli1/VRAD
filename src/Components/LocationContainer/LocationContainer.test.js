df49b12d216c04befa750f028a67f144cae432bb
Erik Raith <raithhtiar@gmail.com>
2020-03-30 10:49:57 -0600

    [fix] Test for LocationContainer await

--- src/Components/LocationContainer/LocationContainer.test.js
+++ src/Components/LocationContainer/LocationContainer.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent, waitFor, wait } from "@testing-library/react";
+import { render, fireEvent, wait } from "@testing-library/react";
 import LocationContainer from "./LocationContainer";
 import "@testing-library/jest-dom";
 import {fetchLocations} from "../../ApiCalls/ApiCalls.js";
@@ -11,41 +11,38 @@ describe("Test", () => {
   let favoriteLocations;
   let response;
   beforeEach(() => {
-    listingDetails = ["/api/v1/listings/3", "/api/v1/listings/44"];
+    listingDetails = ['/api/v1/listings/3','/api/v1/listings/44']
     favoriteLocations = [3];
 
-    response = [
-      {
-        listing_id: 3,
-        area_id: 590,
-        name: "Hip RiNo Party Spot",
-        address: {
-          street: "2250 Lawrence St",
-          zip: "80205"
-        },
-        details: {
-          neighborhood_id: 5124122,
-          superhost: true,
-          seller_source: "91jss1",
-          beds: 3,
-          baths: 2.5,
-          cost_per_night: 420,
-          features: ["hot tub", "espresso machine"]
-        },
-        dev_id: "u4gh2j",
-        area: "rino",
-        db_connect: 834470
-      }
-    ];
+    response =
+        [{
+            "listing_id": 3,
+            "area_id": 590,
+            "name": "Hip RiNo Party Spot",
+            "address": {
+                "street": "2250 Lawrence St",
+                "zip": "80205"
+            },
+            "details": {
+                "neighborhood_id": 5124122,
+                "superhost": true,
+                "seller_source": "91jss1",
+                "beds": 3,
+                "baths": 2.5,
+                "cost_per_night": 420,
+                "features": [
+                    "hot tub",
+                    "espresso machine"
+                ]
+            },
+            "dev_id": "u4gh2j",
+            "area": "rino",
+            "db_connect": 834470
+        }]
+
   });
-  beforeAll(async(done)=>{
-    done();
-  })
-  afterAll(async(done)=>{
-    done();
-  })
-  it.skip("should display the locations info", async (done) => {
-    expect.assertions(1)
+
+  it("should display the locations info", async () => {
     const mockFavorite = jest.fn();
     const mockIsFavorite = jest.fn();
     const mockChangeView = jest.fn();
@@ -61,9 +58,11 @@ describe("Test", () => {
         />
       </BrowserRouter>
     );
+    await wait(()=> expect(getByText("Hip RiNo Party Spot")).toBeInTheDocument())
+    // const locationAddressEl = getByText("2250 Lawrence St");
+    //
+    // expect(locationNameEl).toBeInTheDocument();
+    // expect(locationAddressEl).toBeInTheDocument();
 
-    debug();
-
-    await expect(getByText("Hip RiN Party Spot")).toBeInTheDocument();
-  },1000);
+  });
 });
